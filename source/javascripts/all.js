dc202510aa3cb5d9a357c93649b822e1017ea61b
Gurvir Cheema <gurvircheema@hotmail.com>
2016-07-18 14:02:19 -0700

    max lossless compression

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -63,40 +63,7 @@ $(document).ready(function(){
       $('.intro').css({
         'transform': 'translate(0px, -' + wScroll/40 + '%)'
       });
-
-      // if( wScroll > $('.row').offset().top - ($(window).height()/1.7)){
-      //   check_if_in_view();
-      // }
     });
-  }else {
   }
-
 })
 
-
-
-
-// function check_if_in_view() {
-//   var $animation_elements = $('.animation-element');
-//   var $window = $(window);
-//   var window_height = $window.height();
-//   var window_top_position = $window.scrollTop();
-//   var window_bottom_position = (window_top_position + window_height);
-//
-//   $.each($animation_elements, function() {
-//     var $element = $(this);
-//     var element_height = $element.outerHeight();
-//     var element_top_position = $element.offset().top;
-//     var element_bottom_position = (element_top_position + element_height);
-//
-//     //check to see if this current container is within viewport
-//     if ((element_bottom_position >= window_top_position) &&
-//         (element_top_position <= window_bottom_position)) {
-//       // $element.slideDown('slow');
-//       // $element.addClass('in-view slide-right').removeClass('slide-left');
-//     } else {
-//       // $element.addClass('slide-left').removeClass('in-view slide-right');
-//       // $element.slide('slow');
-//     }
-//   });
-// }
